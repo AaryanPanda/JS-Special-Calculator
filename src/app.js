e0d72edcd94a46b0f0f7c1110a622f6ecc1ae8be
aaryan <aaryanpanda04@gmail.com>
2024-01-15 17:52:46 +0530

    Created the necessary functions with proper arguments

--- src/app.js
+++ src/app.js
@@ -2,6 +2,38 @@
 
 // function specialAddition(){}
 
-// Progression 2: Create simple division
-
-// Progression 3: Create the special calculator
+const specialAddition = function(numberOne,numberTwo){
+    if(numberOne<0){
+      return 'Negative numbers are not allowed'
+    }
+    else if(numberTwo<0){
+      return 'Negative numbers are not allowed'
+    }
+    else{
+      return numberOne-numberTwo;
+    }
+  }
+  
+  // Progression 2: Create simple division
+  
+  const simpleDivision = function(numberOne,numberTwo){
+    if (numberTwo>numberOne){
+      return 'Cannot divide a smaller number by a larger number'
+    }
+    else if(numberTwo<0){
+      return 'Negative numbers are not allowed'
+    }
+    else if (numberTwo==0){
+      return 'Not a number'
+    }
+    else{
+      return numberOne/numberTwo;
+    }
+  }
+  
+  // Progression 3: Create the special calculator
+  
+  const specialCalculator = function(numberOne,numberTwo,operation){
+    return operation(numberOne,numberTwo);
+  }
+  
